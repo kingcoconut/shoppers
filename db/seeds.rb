5d8a53717c3ae76791153de03a83110e03030b24
Xander Groeneveld <xander.groeneveld@gmail.com>
2017-06-04 11:28:07 +1000

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+2000.times do
+  applicant = Applicant.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    cell: Faker::PhoneNumber.cell_phone,
+    zipcode: Faker::Address.zip.split("-")[0],
+    background_consent: rand(1..10) > 2,
+    created_at: Time.now - (rand(0..52).weeks),
+    aasm_state: "applied"
+  )
+  if applicant.background_consent
+    applicant.aasm_state = $APPLICANT_STATES[rand(0..5)]
+  end
+  applicant.save
+end
